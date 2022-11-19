16eb739ede0d59707f076641b811305f8ae31526
henrysoares <henrysoares.dev@gmail.com>
2022-11-18 23:09:47 -0300

    Adding initial files

---
+++ simple-jobs/src/test/java/com/henrysoares/simplejobs/SimpleJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrysoares.simplejobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
